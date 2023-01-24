ce03a82118b4caaab3a7c45e22d2631737c05326
Saumil Gauswami <saumil@lapisinfo.com>
2023-01-24 14:45:18 +0530

    [ADD] added fid manager module and modified report

--- invoice_ext/__manifest__.py
+++ invoice_ext/__manifest__.py
@@ -1,15 +1,13 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Facture Extension",
-    'summary': """ invoice_ext """,
-    'description': """ invoice_ext """,
+    'summary': """ Facture Extension """,
     'author': "DG",
     'category': 'account',
-    'version': '16.0.1.0.0',
     'license': 'LGPL-3',
+    'version': '16.0.1.0.0',
     'depends': ['base', 'account', 'web', 'base_setup'],
     'data': [
-        'views/templates.xml',
         'report/account_report.xml',
         'report/report_invoice.xml',
     ],
