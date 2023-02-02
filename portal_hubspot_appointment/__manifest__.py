5f3fd5491c75022ac326a38fd31698ecd6ae67ac
Saumil Gauswami <saumil@lapisinfo.com>
2023-02-02 11:28:08 +0530

    [IMP] Added hubspot link in portal

---
+++ portal_hubspot_appointment/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+	'name': "Portal Hubspot Appointment",
+	'summary': """ This module will let you make an appointment from portal side. """,
+	'category': 'portal',
+	'version': '16.0.1.0.0',
+	'depends': ['base', 'web', 'portal'],
+	'data': [
+		'views/hubspot_template.xml',
+	]
+}
+
