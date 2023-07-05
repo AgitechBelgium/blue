80598c2e3fa44015c12128c9a8fe5acb39d79282
Saumil Gauswami <saumil@lapisinfo.com>
2023-07-05 18:51:33 +0530

    [FIX] label changed for hubspot meeting

--- portal_hubspot_appointment/__manifest__.py
+++ portal_hubspot_appointment/__manifest__.py
@@ -3,7 +3,7 @@
 	'name': "Portal Hubspot Appointment",
 	'summary': """ This module will let you make an appointment from portal side. """,
 	'category': 'portal',
-	'version': '16.0.1.0.0',
+	'version': '16.0.1.0.1',
 	'depends': ['base', 'web', 'portal'],
 	'data': [
 		'views/hubspot_template.xml',
